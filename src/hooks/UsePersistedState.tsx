75e7dd640b31484c32dff5bbaeb4c15b05e254db
ben <b.harding@gmail.com>
2020-11-20 17:49:59 -0800

    adding option intializer for useState

--- src/hooks/UsePersistedState.tsx
+++ src/hooks/UsePersistedState.tsx
@@ -1,11 +1,11 @@
 import * as React from 'react';
 import {useState, Dispatch, SetStateAction} from 'react';
  
-export function usePersistedState<T>(localStorageKey: string): [T, Dispatch<SetStateAction<T>>] {
-  const [value, setValue] = useState<T>(JSON.parse(localStorage.getItem(localStorageKey)));
+export function usePersistedState<T>(key: string, init?: T): [T, Dispatch<SetStateAction<T>>] {
+  const [value, setValue] = useState<T>(JSON.parse(localStorage.getItem(key)) || init);
  
   React.useEffect(() => {
-    localStorage.setItem(localStorageKey, JSON.stringify(value));
+    localStorage.setItem(key, JSON.stringify(value));
   }, [value]);
  
   return [value, setValue];
