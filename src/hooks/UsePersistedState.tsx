6e24b310f25caee976dd4c7bdf05a10499872820
ben <b.harding@gmail.com>
2020-12-22 14:47:03 -0800

    tweaking persisted state hook to allow for new entries from static files

--- src/hooks/UsePersistedState.tsx
+++ src/hooks/UsePersistedState.tsx
@@ -4,7 +4,9 @@ import {useState, useEffect, Dispatch, SetStateAction} from 'react';
 export type PersistedState<T> = [T, Dispatch<SetStateAction<T>>];
 
 export function usePersistedState<T>(key: string, init?: T): PersistedState<T> {
-    const [value, setValue] = useState<T>(JSON.parse(localStorage.getItem(key)) || init);
+    const initialState = Object.assign(init || {}, JSON.parse(localStorage.getItem(key)) || {});
+
+    const [value, setValue] = useState<T>(initialState);
 
     useEffect(() => {
         localStorage.setItem(key, JSON.stringify(value));
