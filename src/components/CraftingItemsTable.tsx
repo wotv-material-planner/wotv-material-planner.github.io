0979c3d2a666bcd7c02611fa4142511832752af0
ben <b.harding@gmail.com>
2020-12-29 16:29:12 -0800

    select for crafting item types

--- src/components/CraftingItemsTable.tsx
+++ src/components/CraftingItemsTable.tsx
@@ -6,24 +6,44 @@ import './CraftingItemsTable.scss';
 
 export const CraftingItemsTable: FunctionComponent = () => {
     const [craftingItems, setCraftingItems] = useContext(UserCraftingItemsContext);
-    const {artifactMap} = useContext(WotvDumpContext);
+    const {artifactMap, typeMap} = useContext(WotvDumpContext);
+
+    const buildCraftingItemRow = (craftingItem: CraftingItem, itemIndex) => {
+        const artifact = artifactMap[craftingItem.iname];
+
+        return (
+            <div
+                key={`craftingItem-${itemIndex}`}
+            >
+                <div>{artifact.name}</div>
+                <select
+                    value={craftingItem.targetGrowthType}
+                    onChange={(e) => {
+                        const newCraftingItems = [...craftingItems];
+                        craftingItems[itemIndex].targetGrowthType = e.target.value;
+
+                        setCraftingItems(newCraftingItems);
+                    }}
+                >
+                    <option value="" />
+                    {typeMap[artifact.rtype].map((typeOptions, index) => {
+                        return (
+                            <option
+                                key={`craftingItem-${itemIndex}-option-${index}`}
+                            >
+                                {typeOptions.label}
+                            </option>
+                        )
+                    })}
+                </select>
+            </div>
+        );
+    }
 
     return (
         <div className="CraftingItemsTable">
             <div>Crafting Items</div>
-            {
-                craftingItems.map((craftingItem: CraftingItem, index) => {
-                    const artifact = artifactMap[craftingItem.iname];
-
-                    return (
-                        <div
-                            key={`craftingItem-${index}`}
-                        >
-                            {artifact.name}
-                        </div>
-                    );
-                })
-            }
+            {craftingItems.map(buildCraftingItemRow)}
         </div>
     );
 };
