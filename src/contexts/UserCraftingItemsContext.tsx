eb2386629065407fdbb230b8916bb97dd6337d64
ben <b.harding@gmail.com>
2021-01-06 16:02:22 -0800

    yanking the crafting element functions out of the component

--- src/contexts/UserCraftingItemsContext.tsx
+++ src/contexts/UserCraftingItemsContext.tsx
@@ -4,7 +4,7 @@ import {usePersistedState, PersistedState} from '../hooks/UsePersistedState';
 import {BookMap, UserBooksContext} from './UserBooksContext';
 import {MaterialMap, UserMaterialsContext} from './UserMaterialsContext';
 import {RecipeMap, UserRecipesContext} from './UserRecipesContext';
-import {WotvDumpContext} from './WotvDumpContext';
+import {DumpContext, WotvDumpContext} from './WotvDumpContext';
 
 export interface CraftingItem {
     iname: string;
@@ -24,131 +24,132 @@ interface TotalCraftingElements {
     materials: CraftingElementMap;
 };
 
-export const UserCraftingItemsContext = createContext<PersistedState<CraftingItem[]>>([null, null]);
-
 interface ProviderProps {
     init?: CraftingItem[];
 };
 
+export const UserCraftingItemsContext = createContext<PersistedState<CraftingItem[]>>([null, null]);
+
+export const getItemCraftingElements = (iname: string, level: number, wotvDump: DumpContext): CraftingElementMap => {
+    const elements: CraftingElementMap = {};
+    const {artifactRecipeMap, artifactAwakeMap} = wotvDump;
+
+    if (artifactRecipeMap[iname]) {
+        artifactRecipeMap[iname].craft.forEach((craftingElement) => {
+            if (craftingElement.type === 0) {
+                if (!elements[craftingElement.id]) {
+                    elements[craftingElement.id] = +craftingElement.num;
+                } else {
+                    elements[craftingElement.id] += +craftingElement.num;
+                }
+
+            } else {
+                let itemElements = getItemCraftingElements(craftingElement.id, craftingElement.lv, wotvDump);
+
+                for (let iname in itemElements) {
+                    if (!elements[iname]) {
+                        elements[iname] = itemElements[iname];
+                    } else {
+                        elements[iname] += itemElements[iname];
+                    }
+                }
+            }
+        });
+    }
+
+    if (artifactAwakeMap[iname]) {
+        artifactAwakeMap[iname].awakes.forEach((awake) => {
+            if (awake.lv <= level) {
+                let matIndex = 1;
+
+                while (awake['mat' + matIndex] && matIndex <= 9) {
+                    let mat = awake['mat' + matIndex].split(',');
+
+                    if (elements[mat[0]]) {
+                        elements[mat[0]] += +mat[1];
+                    } else {
+                        elements[mat[0]] = +mat[1];
+                    }
+
+                    matIndex++;
+                }
+            }
+        });
+    }
+
+    return elements;
+};
+
+export const getTotalCraftingElements = (items: CraftingItem[], wotvDump: DumpContext): TotalCraftingElements => {
+    return items.reduce((acc: TotalCraftingElements, curr: CraftingItem) => {
+        let targetPlusIname = curr.iname;
+
+        if (curr.targetPlus) {
+            targetPlusIname = `${curr.iname}_${curr.targetPlus}`;
+        }
+
+        const targetPlusElements = getItemCraftingElements(targetPlusIname, curr.targetPlus * 10, wotvDump);
+
+        let currentPlusElements: CraftingElementMap = {};
+
+        if (curr.currentPlus !== null) {
+            let currentPlusIname = curr.iname;
+
+            if (curr.currentPlus) {
+                currentPlusIname = `${curr.iname}_${curr.currentPlus}`;
+            }
+
+            currentPlusElements = getItemCraftingElements(currentPlusIname, curr.currentPlus * 10, wotvDump);
+        }
+
+        for (let iname in targetPlusElements) {
+            const needed = targetPlusElements[iname] - (currentPlusElements[iname] || 0);
+
+            if (iname.startsWith('IT_AF_AW')) {
+                if (!acc.books[iname]) {
+                    acc.books[iname] = needed;
+                } else {
+                    acc.books[iname] += needed;
+                }
+            }
+
+            if (iname.startsWith('IT_AF_LW')) {
+                if (!acc.recipes[iname]) {
+                    acc.recipes[iname] = needed;
+                } else {
+                    acc.recipes[iname] += needed;
+                }
+            }
+
+            if (iname.startsWith('IT_AF_MAT')) {
+                if (!acc.materials[iname]) {
+                    acc.materials[iname] = needed;
+                } else {
+                    acc.materials[iname] = needed;
+                }
+            }
+        }
+
+        return acc;
+    }, {books: {}, recipes: {}, materials: {}} as TotalCraftingElements);
+};
+
 export const UserCraftingItemsProvider: FunctionComponent<ProviderProps> = (props) => {
-    const defaultContext: PersistedState<CraftingItem[]> = 
+    const defaultContext: PersistedState<CraftingItem[]> =
         usePersistedState<CraftingItem[]>('userCraftingItems', props.init || []);
 
     const [craftingItems, setCraftingItems] = defaultContext;
     const [books, setBooks] = useContext(UserBooksContext);
     const [recipes, setRecipes] = useContext(UserRecipesContext);
     const [materials, setMaterials] = useContext(UserMaterialsContext);
-    const {artifactRecipeMap, artifactAwakeMap} = useContext(WotvDumpContext);
-
-    const getItemElements = (iname: string, level: number): CraftingElementMap => {
-        const elements: CraftingElementMap = {};
-
-        if (artifactRecipeMap[iname]) {
-            artifactRecipeMap[iname].craft.forEach((craftingElement) => {
-                if (craftingElement.type === 0) {
-                    if (!elements[craftingElement.id]) {
-                        elements[craftingElement.id] = +craftingElement.num;
-                    } else {
-                        elements[craftingElement.id] += +craftingElement.num;
-                    }
-
-                } else {
-                    let itemElements = getItemElements(craftingElement.id, craftingElement.lv);
-
-                    for (let iname in itemElements) {
-                        if (!elements[iname]) {
-                            elements[iname] = itemElements[iname];
-                        } else {
-                            elements[iname] += itemElements[iname];
-                        }
-                    }
-                }
-            });
-        }
-
-        if (artifactAwakeMap[iname]) {
-            artifactAwakeMap[iname].awakes.forEach((awake) => {
-                if (awake.lv <= level) {
-                    let matIndex = 1;
-
-                    while (awake['mat' + matIndex] && matIndex <= 9) {
-                        let mat = awake['mat' + matIndex].split(',');
-
-                        if (elements[mat[0]]) {
-                            elements[mat[0]] += +mat[1];
-                        } else {
-                            elements[mat[0]] = +mat[1];
-                        }
-
-                        matIndex++;
-                    }
-                }
-            });
-        }
-
-        return elements;
-    };
-
-    const getTotalElements = (items: CraftingItem[]): TotalCraftingElements => {
-        return craftingItems.reduce((acc: TotalCraftingElements, curr: CraftingItem) => {
-            let targetPlusIname = curr.iname;
-
-            if (curr.targetPlus) {
-                targetPlusIname = `${curr.iname}_${curr.targetPlus}`;
-            }
-
-            const targetPlusElements = getItemElements(targetPlusIname, curr.targetPlus * 10);
-
-            let currentPlusElements: CraftingElementMap = {};
-
-            if (curr.currentPlus !== null) {
-                let currentPlusIname = curr.iname;
-
-                if (curr.currentPlus) {
-                    currentPlusIname = `${curr.iname}_${curr.currentPlus}`;
-                }
-
-                currentPlusElements = getItemElements(currentPlusIname, curr.currentPlus * 10);
-            }
-
-            for (let iname in targetPlusElements) {
-                const needed = targetPlusElements[iname] - (currentPlusElements[iname] || 0);
-
-                if (iname.startsWith('IT_AF_AW')) {
-                    if (!acc.books[iname]) {
-                        acc.books[iname] = needed;
-                    } else {
-                        acc.books[iname] += needed;
-                    }
-                }
-
-                if (iname.startsWith('IT_AF_LW')) {
-                    if (!acc.recipes[iname]) {
-                        acc.recipes[iname] = needed;
-                    } else {
-                        acc.recipes[iname] += needed;
-                    }
-                }
-
-                if (iname.startsWith('IT_AF_MAT')) {
-                    if (!acc.materials[iname]) {
-                        acc.materials[iname] = needed;
-                    } else {
-                        acc.materials[iname] = needed;
-                    }
-                }
-            }
-
-            return acc;
-        }, {books: {}, recipes: {}, materials: {}} as TotalCraftingElements);
-    };
+    const wotvDump = useContext(WotvDumpContext);
 
     useEffect(() => {
         const newBooks: BookMap = {...books};
         const newRecipes: RecipeMap = {...recipes};
         const newMaterials: MaterialMap = {...materials};
 
-        const totalElements = getTotalElements(craftingItems);
+        const totalElements = getTotalCraftingElements(craftingItems, wotvDump);
 
         for (let book in totalElements.books) {
             newBooks[book].totalNeeded = totalElements.books[book];
