1a79dd79ba6e25230377b40bd3984c148f8e92d6
ben <b.harding@gmail.com>
2021-01-07 14:30:32 -0800

    minor recipe change handler adjustment, adding books to item row

--- src/components/CraftingItemsTable.tsx
+++ src/components/CraftingItemsTable.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import {FunctionComponent, useContext, ChangeEvent} from 'react';
+import {BookMap, UserBooksContext} from '../contexts/UserBooksContext';
 import {CraftingItem, getTotalCraftingElements, UserCraftingItemsContext} from '../contexts/UserCraftingItemsContext';
 import {RecipeMap, UserRecipesContext} from '../contexts/UserRecipesContext';
 import {WotvDumpContext} from '../contexts/WotvDumpContext';
@@ -8,14 +9,25 @@ import './CraftingItemsTable.scss';
 export const CraftingItemsTable: FunctionComponent = () => {
     const [craftingItems, setCraftingItems] = useContext(UserCraftingItemsContext);
     const [recipes, setRecipes] = useContext(UserRecipesContext);
+    const [books, setBooks] = useContext(UserBooksContext);
     const wotvDump = useContext(WotvDumpContext);
-    const {artifactMap, typeMap} = wotvDump;
+    const {artifactMap, itemNameMap, typeMap} = wotvDump;
 
     const buildCraftingItemRow = (craftingItem: CraftingItem, itemIndex) => {
         const artifact = artifactMap[craftingItem.iname];
 
         let totalElements = getTotalCraftingElements([craftingItem], wotvDump);
+
         const recipe = Object.keys(totalElements.recipes)[0];
+
+        const book = Object.keys(totalElements.books)[0];
+        const bookType = itemNameMap[book]?.replace(/.*\(|\).*/g, '');
+
+        const craftingMaterials = Object.keys(totalElements.materials);
+        const material1 = craftingMaterials[0];
+        const material2 = craftingMaterials[1];
+        const material3 = craftingMaterials[2];
+        const material4 = craftingMaterials[3];
 
         return (
             <div
@@ -106,16 +118,46 @@ export const CraftingItemsTable: FunctionComponent = () => {
                     {recipe &&
                         <>
                             <input
+                                placeholder={`${artifact.name} recipes`}
                                 defaultValue={recipes[recipe].current}
                                 onChange={(event: ChangeEvent<HTMLInputElement>) => {
                                     const newRecipes: RecipeMap = {...recipes};
-                                    newRecipes[recipe].current = +event.target.value;
+
+                                    if (event.target.value === '') {
+                                        newRecipes[recipe].current = null;
+                                    } else {
+                                        newRecipes[recipe].current = +event.target.value;
+                                    }
 
                                     setRecipes(newRecipes);
                                 }}
                             />
                             <div>
-                                / {totalElements.recipes[recipe]}
+                                / {totalElements.recipes[recipe] ?? 0}
+                            </div>
+                        </>
+                    }
+                </div>
+                <div>
+                    {book &&
+                        <>
+                            <input
+                                placeholder={`${bookType} books`}
+                                defaultValue={books[book].current}
+                                onChange={(event: ChangeEvent<HTMLInputElement>) => {
+                                    const newBooks: BookMap = {...books};
+
+                                    if (event.target.value === '') {
+                                        newBooks[book].current = null;
+                                    } else {
+                                        newBooks[book].current = +event.target.value;
+                                    }
+
+                                    setBooks(newBooks);
+                                }}
+                            />
+                            <div>
+                                / {totalElements.books[book] ?? 0}
                             </div>
                         </>
                     }
