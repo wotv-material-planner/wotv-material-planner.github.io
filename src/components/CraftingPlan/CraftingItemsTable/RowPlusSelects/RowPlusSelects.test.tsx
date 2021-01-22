7e6db3148aa51d70a7a8c77d2a0eeffb77a43351
ben <b.harding@gmail.com>
2021-01-22 14:58:51 -0800

    RowPlusSelect tests

--- src/components/CraftingPlan/CraftingItemsTable/RowPlusSelects/RowPlusSelects.test.tsx
+++ src/components/CraftingPlan/CraftingItemsTable/RowPlusSelects/RowPlusSelects.test.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
-import {render, screen} from '@testing-library/react';
+import {fireEvent, render, screen} from '@testing-library/react';
 import {RowPlusSelects} from './RowPlusSelects';
-import {CraftingItem, UserCraftingItemsContext, UserCraftingItemsProvider} from '~contexts/UserCraftingItemsContext';
+import {CraftingItem, UserCraftingItemsContext} from '~contexts/UserCraftingItemsContext';
 import {arbitraryCraftingItem} from '~testSupport/arbitraryObjects';
 import {UserBooksProvider} from '~contexts/UserBooksContext';
 import {UserRecipesProvider} from '~contexts/UserRecipesContext';
@@ -11,6 +11,141 @@ import {PersistedState} from '~hooks/UsePersistedState';
 describe('RowPlusSelects', () => {
     it('renders a RowPlusSelects', () => {
         renderSubject({});
+    });
+
+    it('renders two selects for the craftingItem', () => {
+        const craftingItems: CraftingItem[] = [{
+            ...arbitraryCraftingItem(),
+        }];
+
+        renderSubject({value: [craftingItems, null]});
+
+        expect(screen.getAllByRole('combobox')).toHaveLength(2);
+    });
+
+    it('renders a Current plusSelect for the craftingItem', () => {
+        const craftingItems: CraftingItem[] = [{
+            ...arbitraryCraftingItem(),
+            currentPlus: 1,
+        }];
+
+        renderSubject({value: [craftingItems, null]});
+
+        expect(screen.getByText('Current')).toBeTruthy();
+        expect(screen.getByDisplayValue('+1')).toBeTruthy();
+    });
+
+    it('renders a Target plusSelect for the craftingItem', () => {
+        const craftingItems: CraftingItem[] = [{
+            ...arbitraryCraftingItem(),
+            targetPlus: 2,
+        }];
+
+        renderSubject({value: [craftingItems, null]});
+
+        expect(screen.getByText('Target')).toBeTruthy();
+        expect(screen.getByDisplayValue('+2')).toBeTruthy();
+    });
+
+    it('doesnt render plus selects when displaySelect is false', () => {
+        const craftingItems: CraftingItem[] = [{
+            ...arbitraryCraftingItem(),
+            currentPlus: 1,
+            targetPlus: 2,
+        }];
+
+        renderSubject({displaySelect: false, value: [craftingItems, null]});
+
+        expect(screen.queryByDisplayValue('+1')).toBeFalsy();
+        expect(screen.queryByDisplayValue('+2')).toBeFalsy();
+    });
+
+    it('sets the currentPlus value when select is changed', () => {
+        const craftingItems: CraftingItem[] = [{
+            ...arbitraryCraftingItem(),
+            currentPlus: 1,
+            targetPlus: 5,
+        }];
+        const setCraftingItems = jest.fn();
+
+        const expectedCraftingItems: CraftingItem[] = [{
+            ...arbitraryCraftingItem(),
+            currentPlus: 4,
+            targetPlus: 5,
+        }];
+
+        renderSubject({value: [craftingItems, setCraftingItems]});
+
+        const plusSelects = screen.getAllByRole('combobox');
+        fireEvent.change(plusSelects[0], {target: {value: 4}});
+
+        expect(setCraftingItems).toHaveBeenCalledWith(expectedCraftingItems);
+    });
+
+    it('adjusts the targetPlus when the currentPlus is changed and is larger than the targetPlus', () => {
+        const craftingItems: CraftingItem[] = [{
+            ...arbitraryCraftingItem(),
+            currentPlus: 0,
+            targetPlus: 2,
+        }];
+        const setCraftingItems = jest.fn();
+
+        const expectedCraftingItems: CraftingItem[] = [{
+            ...arbitraryCraftingItem(),
+            currentPlus: 3,
+            targetPlus: 3,
+        }];
+
+        renderSubject({value: [craftingItems, setCraftingItems]});
+
+        const plusSelects = screen.getAllByRole('combobox');
+        fireEvent.change(plusSelects[0], {target: {value: 3}});
+
+        expect(setCraftingItems).toHaveBeenCalledWith(expectedCraftingItems);
+    });
+
+    it('sets the targetPlus value when select is changed', () => {
+        const craftingItems: CraftingItem[] = [{
+            ...arbitraryCraftingItem(),
+            currentPlus: 1,
+            targetPlus: 3,
+        }];
+        const setCraftingItems = jest.fn();
+
+        const expectedCraftingItems: CraftingItem[] = [{
+            ...arbitraryCraftingItem(),
+            currentPlus: 1,
+            targetPlus: 5,
+        }];
+
+        renderSubject({value: [craftingItems, setCraftingItems]});
+
+        const plusSelects = screen.getAllByRole('combobox');
+        fireEvent.change(plusSelects[1], {target: {value: 5}});
+
+        expect(setCraftingItems).toHaveBeenCalledWith(expectedCraftingItems);
+    });
+
+    it('adjusts the currnetPlus when the targetPlus is changed and is smaller than the currentPlus', () => {
+        const craftingItems: CraftingItem[] = [{
+            ...arbitraryCraftingItem(),
+            currentPlus: 3,
+            targetPlus: 4,
+        }];
+        const setCraftingItems = jest.fn();
+
+        const expectedCraftingItems: CraftingItem[] = [{
+            ...arbitraryCraftingItem(),
+            currentPlus: 2,
+            targetPlus: 2,
+        }];
+
+        renderSubject({value: [craftingItems, setCraftingItems]});
+
+        const plusSelects = screen.getAllByRole('combobox');
+        fireEvent.change(plusSelects[1], {target: {value: 2}});
+
+        expect(setCraftingItems).toHaveBeenCalledWith(expectedCraftingItems);
     });
 });
 
